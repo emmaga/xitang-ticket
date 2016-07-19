989924283b44cb9f591ee5d4b10180b13a11f884
John-Code <John0927@live.com>
2016-07-19 14:53:09 +0800

    商品添加页面更新

--- src/lib/scripts.js
+++ src/lib/scripts.js
@@ -30,16 +30,6 @@ $(document).ready(function() {
     });
 });
 
-$('.form_date').datetimepicker({
-        language:  'zh-CN',
-        weekStart: 1,
-        todayBtn:  1,
-        autoclose: 1,
-        todayHighlight: 1,
-        startView: 2,
-        minView: 2,
-        forceParse: 0
-    });
 
 angular.module('app', ['n3-line-chart'])
 .controller('MyChartCtrl', function($scope) {
