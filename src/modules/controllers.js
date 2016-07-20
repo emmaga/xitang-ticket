b2f2e28a39e74ba4ea525fc787cdc1b7bab74096
emmaga <emma_0812@msn.cn>
2016-07-20 09:59:46 +0800

    分销商设置列表

--- src/modules/controllers.js
+++ src/modules/controllers.js
@@ -141,10 +141,68 @@
     var self = this;
   }]);
 
-  app.controller('partnerConfigController', ['$scope', function($scope) {
-    console.log('partnerConfig');
-    var self = this;
-  }]);
+  app.controller('partnerConfigController', ['$scope', '$http', '$cookies', '$location', '$window', 'NgTableParams', 
+    function($scope, $http, $cookies, $location, $window, NgTableParams) {
+      console.log('partnerConfig');
+      var self = this;
+
+      // ngtable
+      self.search = function() {
+        self.tableParams = new NgTableParams(
+          {
+            page: 1, 
+            count: 15,
+            url: ''
+          }, 
+          {
+            counts: [15, 30],
+            getData: function(params) {
+              var paramsUrl = params.url();
+              var searchName = "";
+              if (self.searchName) {
+                searchName = self.searchName;
+              }
+
+              var c = $scope.root.config;
+              var url = c.requestUrl + '/goods' + c.extension;
+              var data = {
+                "action": "GetList",
+                "account": $cookies.get('account'),
+                "token": $cookies.get('token'),
+                "projectName": $cookies.get('projectName'),
+                "sortBy": "createDate",
+                "asc": "desc",
+                "count": paramsUrl.count, //一页显示数量
+                "page": paramsUrl.page,   //当前页
+                "search": {
+                  "goodsName": searchName //完全匹配查询
+                }
+              };
+              data = JSON.stringify(data);
+              self.loading = true;
+              
+              return $http.post(url, data).then(function successCallback(response) {
+                  var data = response.data;
+                  if(data.rescode === 200) {
+                    self.checkboxes = { 'checked': false, items: {} };
+                    self.loading = false;
+                    params.total(data.goods.totalCount);
+                    self.tableData = data.goods.lists;
+                    return data.goods.lists;
+                  }else if(data.rescode === 401){
+                    $location.path('/index');
+                  }else {
+                    alert(data.errInfo);
+                  }  
+                }, function errorCallback(response) {
+                  alert('加载失败，请重试');
+                });
+            }
+          }
+        );
+      }
+    }
+  ]);
 
   app.controller('personalInfoController', ['$scope', function($scope) {
     console.log('personalInfo');
