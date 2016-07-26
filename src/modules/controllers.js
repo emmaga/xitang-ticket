2638a4b8a35652d1f13337053c93dee4cf159901
John-Code <john0927@live.com>
2016-07-26 15:14:46 +0800

    update

--- src/modules/controllers.js
+++ src/modules/controllers.js
@@ -555,15 +555,15 @@ app.controller('toBeCheckedController', ['$scope', '$http', '$cookies', '$locati
 
       self.init = function() {
         $('.form_datetime').datetimepicker({
-          format: 'dd MM yyyy - hh:ii',
+          // format: 'dd MM yyyy - hh:ii',
           language:  'zh-CN',
           weekStart: 1,
-          todayBtn:  1,
-          autoclose: 1,
-          todayHighlight: 1,
-          startView: 2,
-          minView: 2,
-          forceParse: 0
+        todayBtn:  1,
+    autoclose: 1,
+    todayHighlight: 1,
+    startView: 2,
+    forceParse: 0,
+        showMeridian: 1
         });
       }
 
